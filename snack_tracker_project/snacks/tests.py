8636724b8b57f2203258ebf105724e4ee85a7560
belelelolahi <belalelolahi003@gmail.com>
2021-12-06 16:56:42 +0200

    add the tests

--- snack_tracker_project/snacks/tests.py
+++ snack_tracker_project/snacks/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class SnackTest(TestCase):
+
+    def test_list_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "_base.html")
